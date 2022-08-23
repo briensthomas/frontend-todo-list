7e20337934efddb96a10d7339939afca56aa5b06
Brien Thomas <thomasbrien94@gmail.com>
2022-08-23 11:48:23 -0700

    front end signUpUser is connected, redirects when the response is ok, and stores a cookie for the user

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // import functions and grab DOM elements
-
+import { signUpUser } from './fetch-utils.js';
+const signUpForm = document.getElementById('sign-up');
 // let state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(signUpForm);
+    await signUpUser({
+        firstName: formData.get('firstName'),
+        lastName: formData.get('lastName'),  
+        email: formData.get('email'),
+        password: formData.get('password')
+      
+    });
+});
